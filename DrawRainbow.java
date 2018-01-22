0d515951cd6d0ff6e1e53904ee4bdbf0cfd3fa52
Sema ERTUĞRUL <semaertugrul@windowslive.com>
2018-01-22 21:41:22 +0300

    Create DrawRainbow.java

---
+++ DrawRainbow.java
@@ -0,0 +1,37 @@
+package DrawRainbow;
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JPanel;
+/**
+ *
+ * @author sema
+ */
+public class DrawRainbow extends JPanel{
+    
+    private final static Color VIOLENT= new Color(128,0,128);
+    private final static Color INDIGO = new Color(75,0,130);
+    
+    private Color [] colors = {Color.WHITE,Color.GREEN, Color.YELLOW, Color.ORANGE, Color.RED , Color.BLUE,VIOLENT,INDIGO};
+    
+    public DrawRainbow(){
+    
+    setBackground(Color.WHITE);
+    
+    }
+            
+    public void paintComponent(Graphics g){
+    
+        super.paintComponent(g);
+        
+        int radius = 20;
+           int centerX = getWidth() / 2; //use half width
+           int centerY = getHeight() - 10;//down 10 from top
+            for (int counter = colors.length; counter > 0; counter--){
+                g.setColor(colors[counter - 1]);
+                 g.fillArc(centerX - counter * radius, centerY - counter * radius, counter * radius * 2, counter * radius * 2, 0, 180 );
+            }
+             
+    
+    }
+    
+}
